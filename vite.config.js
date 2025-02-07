30202fb96dee46c0459155455bc311f0f54b5089
Hassan Sattar <mr.iu64@gmail.com>
2025-02-06 21:37:36 -0500

    sajdijsafdsajjfjdsfh

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/favourite-posters/'
+  base: '/favourite-posters/',
+  publicDir: 'public',
+  build: {
+    copyPublicDir: true,
+    assetsDir: 'assets'
+  }
 })
